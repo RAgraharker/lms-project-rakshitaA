11db56cfb42a08f3c38becb0a3df4bf625274b07
RAgraharker <rakshitaagraharker10@gmail.com>
2026-04-28 18:00:55 +0530

    Cleaned repository and removed venv tracking

--- backend/lms_backend/lms_backend/settings.py
+++ backend/lms_backend/lms_backend/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+from decouple import config
+
+
 from pathlib import Path
 from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,8 +82,16 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'brajeshyy5@gmail.com'
-EMAIL_HOST_PASSWORD = 'xnloxlqnvwukedzy'
+
+EMAIL_HOST_USER = config(
+    "EMAIL_HOST_USER"
+)
+
+EMAIL_HOST_PASSWORD = config(
+    "EMAIL_HOST_PASSWORD"
+)
+
+
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
--- backend/lms_backend/users/views.py
+++ backend/lms_backend/users/views.py
@@ -11,7 +11,8 @@ from django.conf import settings
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_str
-
+import os
+from decouple import config
 from .serializers import UserSerializer
 from .models import (
     User,

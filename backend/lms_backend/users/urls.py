cdd7045dbce49f09ee1a6ff6ffb3bd6d5a46a574
Rakshita Agraharker <rakshitaagraharker10@gmail.com>
2026-05-11 22:24:52 +0530

    backend correction

--- backend/lms_backend/users/urls.py
+++ backend/lms_backend/users/urls.py
@@ -24,6 +24,7 @@ from .views import (
     get_all_users,
     delete_user,
     admin_add_user,
+    db_status,
     open_lesson,
     delete_module,
     create_admin,
@@ -71,6 +72,7 @@ urlpatterns = [
     path('add-announcement/', add_announcement),
 
     path('all-users/', get_all_users),
+    path('db-status/', db_status),
     path('delete-user/<int:pk>/', delete_user),
     path('admin-add-user/', admin_add_user),
     path(

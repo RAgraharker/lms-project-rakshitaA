73dd8ac622da1787a0e9a14cbf094398644eb060
Rakshita Agraharker <rakshitaagraharker10@gmail.com>
2026-05-12 11:36:46 +0530

    Final super admin dropdown fix

--- backend/lms_backend/lms_backend/settings.py
+++ backend/lms_backend/lms_backend/settings.py
@@ -80,11 +80,11 @@ WSGI_APPLICATION = 'lms_backend.wsgi.application'
 
 AUTH_USER_MODEL = 'users.User'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
+EMAIL_TIMEOUT = 10
 EMAIL_HOST_USER = config(
     "EMAIL_HOST_USER"
 )

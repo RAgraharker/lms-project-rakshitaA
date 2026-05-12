bf7025933d7c323488123fce520832923c40fcd6
Rakshita Agraharker <rakshitaagraharker10@gmail.com>
2026-05-12 12:36:55 +0530

    Added forgot password debug

--- backend/lms_backend/users/views.py
+++ backend/lms_backend/users/views.py
@@ -739,7 +739,7 @@ def forgot_password(request):
 
     FRONTEND_URL = os.getenv(
         "FRONTEND_URL",
-        "https://your-vercel-app.vercel.app"
+        "https://lms-project-rakshitaa.onrender.com"
     )
 
     reset_link = f"{FRONTEND_URL}/reset-password/{uid}/{token}/"

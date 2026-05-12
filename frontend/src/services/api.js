7e0f014b2e5c9c2d4740e71d616a3748499292a0
Rakshita Agraharker <rakshitaagraharker10@gmail.com>
2026-05-12 17:32:00 +0530

    fixed api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,52 +1,3 @@
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: "https://lms-project-rakshitaa.onrender.com/api",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
-
-// // ✅ Attach JWT token
-// API.interceptors.request.use((req) => {
-//   const publicPaths = [
-//     "/users/login/",
-//     "/users/register/",
-//     "/users/forgot-password/",
-//     "/users/reset-password/",
-//   ];
-
-//   const isPublicPath = publicPaths.some((path) => req.url?.startsWith(path));
-
-//   if (isPublicPath) {
-//     delete req.headers.Authorization;
-//     return req;
-//   }
-
-//   const token = localStorage.getItem("token");
-
-//   if (token) {
-//     req.headers.Authorization = `Bearer ${token}`;
-//   }
-
-//   return req;
-// });
-
-// // ✅ Auto logout on token expiry
-// API.interceptors.response.use(
-//   (res) => res,
-//   (err) => {
-//     if (err.response?.status === 401) {
-//       localStorage.clear();
-//       window.location.href = "/";
-//     }
-//     return Promise.reject(err);
-//   }
-// );
-
-// export default API;
-
 import axios from "axios";
 
 const API = axios.create({
@@ -56,7 +7,23 @@ const API = axios.create({
   },
 });
 
+
+// ✅ Attach JWT token
 API.interceptors.request.use((req) => {
+  const publicPaths = [
+    "/users/login/",
+    "/users/register/",
+    "/users/forgot-password/",
+    "/users/reset-password/",
+  ];
+
+  const isPublicPath = publicPaths.some((path) => req.url?.startsWith(path));
+
+  if (isPublicPath) {
+    delete req.headers.Authorization;
+    return req;
+  }
+
   const token = localStorage.getItem("token");
 
   if (token) {
@@ -66,4 +33,17 @@ API.interceptors.request.use((req) => {
   return req;
 });
 
+// ✅ Auto logout on token expiry
+API.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response?.status === 401) {
+      localStorage.clear();
+      window.location.href = "/";
+    }
+    return Promise.reject(err);
+  }
+);
+
 export default API;
+
